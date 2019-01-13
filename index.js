5659b4cda81e7f6eb820c3c6dab7c6d91d50a0ac
Alexandra1003 <alexandrapanasenko10.03@gmail.com>
2019-01-13 21:36:22 +0200

    add styling and refactor code

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let btnStart = document.querySelector('.btn-start');
 let btnStop = document.querySelector('.btn-stop');
-let btnSend = document.querySelector('.btn-send');
+let leverArray = document.querySelectorAll('.lever');
+let currentSwitch;
+let successMessage = document.querySelector('.success-message');
+let failMessage = document.querySelector('.fail-message');
+
 let socket;
 
 let switchArray = [true, undefined, undefined, undefined];
@@ -9,49 +13,77 @@ function startListen() {
     let currentStateId;
     let targetSwitch = true;
     socket = new WebSocket('ws://178.20.156.145:3000');
+
     socket.addEventListener('message', function (event) {
         if (JSON.parse(event.data).newState === 'poweredOff') {
-            console.log('works', event.data);
+            if (failMessage.style.display === 'block') {
+                failMessage.style.display = 'none';
+            }
+
+            successMessage.style.display = 'block';
+            let token = document.querySelector('.token');
+            token.innerText = JSON.parse(event.data).token;
+            token.style.display = 'block';
+
             socket.close(1000, 'You`ve closed this connection');
         }
+
         if (JSON.parse(event.data).newState === 'poweredOn') {
-            console.log('does not work', event.data);
+            failMessage.style.display = 'block';
             targetSwitch = !targetSwitch;
-            console.log(targetSwitch);
         }
+
         if (JSON.parse(event.data).pulled !== undefined) {
             currentStateId = JSON.parse(event.data).stateId;
-            let currentSwitch = JSON.parse(event.data).pulled;
+            currentSwitch = JSON.parse(event.data).pulled;
 
             if (switchArray[currentSwitch] !== undefined) {
                 switchArray[currentSwitch] = !switchArray[currentSwitch];
+                changePosition(leverArray[currentSwitch], switchArray[currentSwitch]);
             } else {
                 socket.send(JSON.stringify({ 'action': 'check', 'lever1': '0', 'lever2': currentSwitch, 'stateId': currentStateId }));
             }
-            console.log('Message from server ', event.data);
-            console.log(switchArray, JSON.parse(event.data).stateId);
         }
+
         if (JSON.parse(event.data).action === 'check') {
             if (JSON.parse(event.data).newState !== 'poweredOn') {
-                console.log('Answer from server ', event.data);
+                currentSwitchNumber = JSON.parse(event.data).lever2;
+                leverArray[currentSwitchNumber].classList.remove('unknown');
+
                 if (JSON.parse(event.data).same === true) {
-                    switchArray[JSON.parse(event.data).lever2] = switchArray[0];
-                    console.log('Message from server ', event.data);
+                    switchArray[currentSwitchNumber] = switchArray[0];
+
+                    changePosition(leverArray[currentSwitchNumber], switchArray[0]);
                 } else {
-                    switchArray[JSON.parse(event.data).lever2] = !switchArray[0];
-                    console.log('Message from server ', event.data);
+                    switchArray[currentSwitchNumber] = !switchArray[0];
+
+                    changePosition(leverArray[currentSwitchNumber], !switchArray[0]);
                 }
             }
         }
+
         if (switchArray.every((val, i, arr) => val === targetSwitch)) {
             socket.send(JSON.stringify({ action: "powerOff", stateId: currentStateId }));
-            console.log('power off', JSON.parse(event.data).stateId);
         }
     });
 }
 
+function changePosition(lever, value) {
+    if (value) {
+        if (lever.className == 'lever off') {
+            lever.classList.remove('off');
+        }
+        lever.classList.add('on');
+    } else {
+        if (lever.className == 'lever on') {
+            lever.classList.remove('on');
+        }
+        lever.classList.add('off');
+    }
+}
+
 function stopListen() {
-    socket.close(1000, 'You`ve closed this connection')
+    socket.close(1000, 'You`ve closed this connection');
 }
 
 btnStart.addEventListener('click', startListen);
